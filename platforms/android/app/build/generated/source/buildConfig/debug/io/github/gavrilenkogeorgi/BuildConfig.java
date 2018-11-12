b851a1c0161e45657042cb1a1e73f0b1b1ea9f18
Gavrilenko <gavrilenko.georgi@gmail.com>
2018-11-12 14:48:19 +0200

    v1.1.1 build landscape mode test & debug

--- platforms/android/app/build/generated/source/buildConfig/debug/io/github/gavrilenkogeorgi/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/io/github/gavrilenkogeorgi/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "io.github.gavrilenkogeorgi";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 190010100;
-  public static final String VERSION_NAME = "1.1.0-ALPHA2";
+  public static final int VERSION_CODE = 190010101;
+  public static final String VERSION_NAME = "1.1.1-ALPHA3";
 }
