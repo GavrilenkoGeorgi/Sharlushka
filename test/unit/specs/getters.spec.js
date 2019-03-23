27edc8238dde4d34e561abec3b298c39598993f4
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-03-23 15:59:05 +0200

    Mutations and getters tests

--- test/unit/specs/getters.spec.js
+++ test/unit/specs/getters.spec.js
@@ -1,4 +1,5 @@
 import getters from '@/store/getters'
+import defaultState from '@/store/defaultState'
 
 test(`"progressBarState" returns 0 up to 100 percent`, () => {
   const state = {
@@ -20,14 +21,162 @@ test(`"mainButtonIsRolling" returns true of false`, () => {
   }
 })
 
-test(`"getDiceIds" returns array of dice with correct length`, () => {
-  const state = {
-    newTurn: false,
-    rollCount: 1
+test(`"getCurrentGameState" returns an object`, () => {
+  expect(typeof getters.getCurrentGameState(defaultState)).toBe(`object`)
+})
+
+test(`game state has default values set`, () => {
+  // current values
+  const currentStateValues = defaultState()
+  // default values
+  let stateDefaults = {
+    defaultUserName: `Anonymous`,
+    user: {
+      isAuthenticated: false,
+      lastResultSaved: false // current result saved
+    },
+    error: false,
+    currentGameTurn: 1, // game turns counter
+    rollCount: 3, // roll counter for the current turn
+    maxGameTurns: 33, // 6 turns for school, 27 for the game
+    maxPossibleScore: 879, // from all combinations with highest values set to final
+    newTurn: true, // 1st turn in game is new turn
+    schoolCompleted: false, // check if school is completed
+    gameOver: false,
+    zeroCheck: false, // to check if zero was saving during turn
+    schoolScoreTotal: 0, // total school score
+    gameTotal: 0, // total game score
+    debug: false, // Vue Devtools will do
+    combinationArray: [],
+    scoreArray: [{
+      value: ``, // school combinations store module?
+      final: false,
+      id: `ones`,
+      icon: `diceOnes`
+    },
+    {
+      value: ``,
+      final: false,
+      id: `twos`,
+      icon: `diceTwos`
+    },
+    {
+      value: ``,
+      final: false,
+      id: `threes`,
+      icon: `diceThrees`
+    },
+    {
+      value: ``,
+      final: false,
+      id: `fours`,
+      icon: `diceFours`
+    },
+    {
+      value: ``,
+      final: false,
+      id: `fives`,
+      icon: `diceFives`
+    },
+    {
+      value: ``,
+      final: false,
+      id: `sixes`,
+      icon: `diceSixes` // 5
+    },
+    { // game combinations
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `pair`,
+      fullName: `Pair` // 6
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `twoPairs`,
+      fullName: `Two Pairs` // 7
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `threeOfAKind`,
+      fullName: `Three O.A.K` // 8
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `full`,
+      fullName: `Full` // 9
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `quads`,
+      fullName: `Quads` // 10
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `poker`,
+      fullName: `Poker` // 11
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `small`,
+      fullName: `Small` // 12
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `large`,
+      fullName: `Large` // 13
+    },
+    {
+      value: ``,
+      displayValues: [],
+      final: false,
+      id: `chance`,
+      fullName: `Chance` // 14
+    }],
+    diceArray: [{ // dice Array
+      value: `#`,
+      chosen: false,
+      id: `first`,
+      currentIcon: `diceOnes`
+    },
+    {
+      value: `#`,
+      chosen: false,
+      id: `second`,
+      currentIcon: `diceTwos`
+    },
+    {
+      value: `#`,
+      chosen: false,
+      id: `third`,
+      currentIcon: `diceThrees`
+    },
+    {
+      value: `#`,
+      chosen: false,
+      id: `fourth`,
+      currentIcon: `diceFours`
+    },
+    {
+      value: `#`,
+      chosen: false,
+      id: `fifth`,
+      currentIcon: `diceFives`
+    }]
   }
-  if (!state.newTurn && state.rollCount <=2 && state.rollCount != 0) {
-    expect(getters.mainButtonIsRolling(state)).toBeTruthy()
-  } else {
-    expect(getters.mainButtonIsRolling(state)).toBeFalsy()
-  }
+  expect(currentStateValues).toEqual(stateDefaults)
 })
--- test/unit/specs/mutations.spec.js
+++ test/unit/specs/mutations.spec.js
@@ -1,23 +1,112 @@
 import mutations from '@/store/mutations'
-// import { setLastSave } from '@/store/mutations'
+import defaultState from '@/store/defaultState'
 
-test(`"setLastSave" sets "state.lastSave" to true`, () => {
-  const state = {
-    lastSave: false
-  }
-  mutations.setLastSave(state)
-  expect(state.lastSave).toBeTruthy()
+const state = defaultState()
+
+test(`"setLastSave" sets "user.lastResultSaved" to boolean`, () => {
+  mutations.setLastSave(state, true)
+  expect(typeof state.user.lastResultSaved).toBe(`boolean`)
 })
 
-test(`"setErrorMessage" sets "state.error.dialogIsVisible"
-  to true and "state.error.dialogMessage"
-  to typeof 'string'`, () => {
-  const state = {
-    error: {
-      dialogIsVisible: false,
-      dialogMessage: undefined
+test(`"computeScore" returns boolean after score calculation`, () => {
+  let result = mutations.computeScore(state)
+  expect(typeof result).toBe(`boolean`)
+})
+
+test(`"saveResultInStore" saves result with given id`, () => {
+  let resultId = `poker`
+  let result = mutations.saveResultInStore(state, resultId)
+  expect(typeof result).toBe(`boolean`)
+})
+
+test(`"rollDice" rolls dice`, () => {
+  let currentRollCount = state.rollCount
+  let newTurnState = true
+  mutations.rollDice(state)
+  expect(state.rollCount).toBeLessThan(currentRollCount)
+  expect(state.newTurn).not.toBe(newTurnState)
+  for (let dice of state.diceArray) {
+    expect(dice.value).toBeLessThanOrEqual(6)
+    expect(dice.value).toBeGreaterThanOrEqual(1)
+  }
+})
+
+test(`"nextTurn" initiates next turn`, () => {
+  let previousTurn = state.currentGameTurn
+  mutations.nextTurn(state)
+  expect(state.zeroCheck).toBeFalsy()
+  expect(state.currentGameTurn).toBeGreaterThan(previousTurn)
+  for (let dice of state.diceArray) {
+    expect(dice.value).toBe(`#`)
+    expect(dice.chosen).toBeFalsy()
+  }
+  expect(state.combinationArray).toEqual([])
+})
+
+test(`"resetState" correctly resets chosen state values to defaults`, () => {
+  mutations.resetState(state)
+  // you should reset whole game settings
+  // object in store which apparently doesn`t exist
+  expect(state.currentGameTurn).toBe(1)
+  expect(state.rollCount).toBe(3)
+  expect(state.maxGameTurns).toBe(33)
+  expect(state.newTurn).toBeTruthy()
+  expect(state.schoolCompleted).toBeFalsy()
+  expect(state.gameOver).toBeFalsy()
+  expect(state.zeroCheck).toBeFalsy()
+  expect(state.schoolScoreTotal).toBe(0)
+  expect(state.gameTotal).toBe(0)
+  expect(state.combinationArray).toEqual([])
+  for (let result of state.scoreArray) {
+    expect(result.value).toBe(``)
+    expect(result.final).toBeFalsy()
+  }
+  for (let dice of state.diceArray) {
+    expect(dice.value).toBe(`#`)
+    expect(dice.chosen).toBeFalsy()
+  }
+  expect(state.user.lastResultSaved).toBeFalsy()
+})
+
+test(`"setUserIsLoggedIn" sets user login state to boolean`, () => {
+  let currentState = state.user.isAuthenticated
+  // toggle it
+  mutations.setUserIsLoggedIn(state, !currentState)
+  expect(typeof state.user.isAuthenticated).toBe(`boolean`)
+  expect(state.user.isAuthenticated).toEqual(!currentState)
+})
+
+test(`"setLastSave" sets user last save to boolean`, () => {
+  let currentState = state.user.lastResultSaved
+  // toggle it
+  mutations.setLastSave(state, !currentState)
+  expect(typeof state.user.isAuthenticated).toBe(`boolean`)
+  expect(state.user.isAuthenticated).toEqual(!currentState)
+})
+
+test(`"resetGameOver" sets user last save to boolean`, () => {
+  let currentState = state.gameOver
+  // toggle it
+  mutations.resetGameOver(state, !currentState)
+  expect(typeof state.user.isAuthenticated).toBe(`boolean`)
+  expect(state.user.isAuthenticated).toEqual(!currentState)
+})
+
+test(`"clearResultBox" clears result box`, () => {
+  mutations.clearResultBox(state)
+  // clear all temp results in store
+  for (let combination of state.scoreArray) {
+    if (!combination.final) {
+      expect(combination.value).toBe(``)
+    }
+    // deselect all dice
+    for (let dice of state.diceArray) {
+      expect(dice.chosen).toBeFalsy()
     }
   }
+})
+
+test(`"setErrorMessage" sets error message`, () => {
   let error = {
     dialogIsVisible: true,
     dialogMessage: `Some error!`
