bdfb07c43b78a131e0b83df18bc9ca8be25b883e
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-05-09 16:08:19 +0300

    Firestore timestamp setting removed

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,9 +1,3 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-// import firebaseConfig from './firebaseConfig'
-/*
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-} */
-firebase.firestore().settings({timestampsInSnapshots: true})
 export default firebase.firestore()
