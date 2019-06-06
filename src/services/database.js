e57876990899fe2497776eaea68045a7592cd996
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-06-06 17:27:06 +0300

    Package updates

--- src/services/database.js
+++ src/services/database.js
@@ -1,5 +1,3 @@
 import config from './firebaseConfig.js'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-
-export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.firestore()
