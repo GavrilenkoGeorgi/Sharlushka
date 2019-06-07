ca43922adc446c42c020ae66fe536c268065ece4
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-06-07 13:54:51 +0300

    New firebase config

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,9 @@
+// remove this, get the new one
+export default {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+}
